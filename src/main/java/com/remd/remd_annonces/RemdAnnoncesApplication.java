0c741549485a261bf3c5cbe1e64fcdba4f9c3a69
DavePhil <dave.chedjoun@facsciences-uy1.cm>
2023-12-23 09:29:11 +0100

    Initial commit

---
+++ src/main/java/com/remd/remd_annonces/RemdAnnoncesApplication.java
@@ -0,0 +1,13 @@
+package com.remd.remd_annonces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemdAnnoncesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemdAnnoncesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remd/remd_annonces/RemdAnnoncesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remd.remd_annonces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemdAnnoncesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
